2feafda9c850d3118728f3453f24392e61220ebe
tjgrst <tjgrst@gmail.com>
2016-06-29 15:15:41 -0500

    initial commit - changed DB connection string

---
+++ Collaborate-lrn-Py/Collaborate-lrn-Py/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Collaborate_lrn_Py.MvcApplication" Language="C#" %>
---
+++ Collaborate-lrn-Py/Collaborate-lrn-Py/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Collaborate_lrn_Py.Startup))]
+namespace Collaborate_lrn_Py
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
