8fe2d525fed83fb80a1bf886cec8b9ab31664334
tjgrst <tjgrst@gmail.com>
2016-07-08 11:46:31 -0500

    Installed react, ran a working component, and using jquery to display correct or incorrect in quiz. Auto-grading not currently correct.

---
+++ Collaborate-lrn-Py/Collaborate-lrn-Py/Scripts/JSX/GoodJob.jsx
@@ -0,0 +1,12 @@
+﻿var GoodJob = React.createClass({
+    render: function () {
+        return (
+            <div> Good {this.props.status} </div>
+            );
+    }
+});
+
+React.render(
+    <GoodJob status="Job!" ></GoodJob>,
+    document.getElementById("result")
+    );
