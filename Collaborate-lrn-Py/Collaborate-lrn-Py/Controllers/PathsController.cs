e36818961d1cb43a2ca51b4ce5d690f2d02b2407
tjgrst <tjgrst@gmail.com>
2016-07-12 11:24:56 -0500

    added paths controller and create view

---
+++ Collaborate-lrn-Py/Collaborate-lrn-Py/Controllers/PathsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Collaborate_lrn_Py.Models;
+
+namespace Collaborate_lrn_Py.Controllers
+{
+    public class PathsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Paths
+        public ActionResult Index()
+        {
+            var paths = db.Paths.Include(p => p.Creator);
+            return View(paths.ToList());
+        }
+
+        // GET: Paths/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Path path = db.Paths.Find(id);
+            if (path == null)
+            {
+                return HttpNotFound();
+            }
+            return View(path);
+        }
+
+        // GET: Paths/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Paths/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,PathName,Rating,CreatorId")] Path path)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Paths.Add(path);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(path);
+        }
+
+        // GET: Paths/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Path path = db.Paths.Find(id);
+            if (path == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(path);
+        }
+
+        // POST: Paths/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,PathName,Rating,CreatorId")] Path path)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(path).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(path);
+        }
+
+        // GET: Paths/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Path path = db.Paths.Find(id);
+            if (path == null)
+            {
+                return HttpNotFound();
+            }
+            return View(path);
+        }
+
+        // POST: Paths/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Path path = db.Paths.Find(id);
+            db.Paths.Remove(path);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
